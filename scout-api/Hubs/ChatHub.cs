2f9e5677b925e597f6d570e04dd9e5da71694197
Carla <126351324+carla-draghiciu@users.noreply.github.com>
2026-05-02 13:53:13 +0300

    implemented chathub

--- scout-api/Hubs/ChatHub.cs
+++ scout-api/Hubs/ChatHub.cs
@@ -1,6 +1,46 @@
-﻿namespace scout_api.Hubs
+﻿using Microsoft.AspNetCore.SignalR;
+using scout_api.Models;
+using scout_api.Services;
+
+namespace scout_api.Hubs
 {
-    public class ChatHub
+    public class ChatHub : Hub
     {
+        private readonly ChatService chatService;
+
+        public ChatHub(ChatService chatService)
+        {
+            this.chatService = chatService;
+        }
+
+        public async Task JoinRoom(string roomId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
+
+            var chatHistory = await chatService.GetRoomHistoryAsync(roomId);
+            await Clients.Caller.SendAsync("LoadChatHistory", chatHistory);
+        }
+
+        public async Task LeaveRoom(string roomId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomId);
+        }
+
+        public async Task SendMessage(string roomId, int senderId, string senderName, string messageContent)
+        {
+            var messageToSend = new ChatMessage
+            {
+                RoomId = roomId,
+                SenderId = senderId,
+                SenderName = senderName,
+                Content = messageContent,
+                TimeStamp = DateTime.UtcNow
+            };
+
+            await chatService.SaveMessageAsync(messageToSend);
+
+            // broadcast to everyone in the room
+            await Clients.Group(roomId).SendAsync("ReceiveMessage", messageToSend);
+        }
     }
 }
