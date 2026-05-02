69152fba739bf454c90ee3ce15a00c804289d83f
Carla <126351324+carla-draghiciu@users.noreply.github.com>
2026-05-02 20:52:44 +0300

    implemented permission functions

--- scout-app/src/app/services/permission.ts
+++ scout-app/src/app/services/permission.ts
@@ -3,4 +3,20 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
-export class Permission {}
+export class PermissionService {
+  getRole(): string {
+    return localStorage.getItem('role') || '';
+  }
+
+  getPermissions(): string[] {
+    return JSON.parse(localStorage.getItem('permissions') || '[]');
+  }
+
+  hasPermission(permission: string): boolean {
+    return this.getPermissions().includes(permission);
+  }
+
+  isAdmin(): boolean {
+    return this.getRole() === 'Admin';
+  }
+}
