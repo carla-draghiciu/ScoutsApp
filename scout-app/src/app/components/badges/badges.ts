0959eab09bd84c92320365130d5ab6e05053be8c
Carla <126351324+carla-draghiciu@users.noreply.github.com>
2026-05-02 21:07:15 +0300

    can manage badges permission check

--- scout-app/src/app/components/badges/badges.ts
+++ scout-app/src/app/components/badges/badges.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { PermissionService } from '../../services/permission';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-badges',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule, FormsModule],
   templateUrl: './badges.html',
   styleUrl: './badges.css',
 })
-export class Badges {}
+export class Badges {
+  canManageBadges: boolean = false;
+  
+  constructor(private permissionService: PermissionService) { 
+    this.canManageBadges = this.permissionService.hasPermission('manage_badges');
+  }
+}
