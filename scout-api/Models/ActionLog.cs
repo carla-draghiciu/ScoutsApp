cbca46ab38aa4c7c28f38c655b6de496453c740c
Carla <126351324+carla-draghiciu@users.noreply.github.com>
2026-05-04 14:23:16 +0300

    action log model

---
+++ scout-api/Models/ActionLog.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace scout_api.Models
+{
+    [Table("ActionLogs")]
+    public class ActionLog
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string UserRole { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(255)]
+        public string Action { get; set; } = string.Empty;
+
+        [MaxLength(500)]
+        public string? Details { get; set; }
+
+        [Required]
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        [MaxLength(50)]
+        public string Endpoint { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(10)]
+        public string HttpMethod { get; set; } = string.Empty;
+    }
+}
