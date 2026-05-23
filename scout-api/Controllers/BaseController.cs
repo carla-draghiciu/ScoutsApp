70ce143da12d83bd8593aed1eb3f943fbcd4b938
Carla <126351324+carla-draghiciu@users.noreply.github.com>
2026-05-23 18:36:33 +0300

    created a base controller

---
+++ scout-api/Controllers/BaseController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using scout_api.Models;
+using scout_api.Services;
+
+namespace scout_api.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        protected readonly UserService userService;
+
+        public BaseController(UserService userService)
+        {
+            this.userService = userService;
+        }
+
+        protected User? GetCurrentUser()
+        {
+            var token = Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+            return userService.GetUserByToken(token);
+        }
+
+        protected bool HasPermission(User user, string permission)
+        {
+            return user.Role.RolePermissions.Any(rp => rp.Permission.Name == permission);
+        }
+
+        protected IActionResult? CheckPermission(string permission)
+        {
+            var currentUser = GetCurrentUser();
+            if (currentUser == null)
+            {
+                return Unauthorized();
+            }
+
+            if (!HasPermission(currentUser, permission))
+            {
+                return Forbid();
+            }
+
+            return null;
+        }
+    }
+}
